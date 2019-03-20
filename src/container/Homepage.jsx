56ba7d8ce1d6e1023a4762da113987c8289e649d
Mimi Lau <mimi5x5@gmail.com>
2019-03-20 19:16:27 +0800

    first commit

---
+++ src/container/Homepage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="Homepage">
+        <header className="header">
+          Users
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
