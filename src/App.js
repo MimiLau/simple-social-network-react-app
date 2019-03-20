f562bacb8bd66ad67ec4f91473ce3a1426212f3b
Mimi Lau <mimi5x5@gmail.com>
2019-03-20 19:16:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import Homepage from './container/Homepage';
+
+import './style/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+				<Homepage />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
